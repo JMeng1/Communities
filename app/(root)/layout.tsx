c7b554ef1152d13902752d505e788989f2a3ec79
Joey Meng <mengjoey@msu.edu>
2023-10-03 12:40:27 -0400

    Added Logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,11 @@ import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import BottomBar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import TopBar from '@/components/shared/TopBar'
+import RightSidebar from '@/components/shared/RightSidebar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +23,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TopBar />
+        <main>
+          <LeftSidebar />
+          <section className='main-container'>
+            <div className='w-full max-w-4xl'>
+              {children}
+            </div>
+          </section>
+        </main>
+        <RightSidebar />
+        <BottomBar />
+        </body>
     </html>
     </ClerkProvider>
   )
